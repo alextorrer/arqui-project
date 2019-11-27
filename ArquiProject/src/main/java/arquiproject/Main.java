50ea4e188ac27968b352c747fa1a0242c8f317fa
jimmy <jimmy@JimmyOjeda>
2019-11-26 18:08:14 -0600

    Controller 1

--- ArquiProject/src/main/java/arquiproject/Main.java
+++ ArquiProject/src/main/java/arquiproject/Main.java
@@ -1,11 +1,56 @@
 
 package arquiproject;
 
+import Controller.Controller;
+import UI.MainUI;
+import UI.VentanaTemperatura;
+import app.Com;
+import app.Parameters;
+import core.SerialPort;
+import java.util.List;
+import static serialPortReader.SerialPortReader.convertirACelsius;
+import static serialPortReader.SerialPortReader.puertosDisponibles;
+
 public class Main {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
+        Controller controller = new Controller();
+        MainUI mainUI = new MainUI();
+        VentanaTemperatura ventanaTemperatura = new VentanaTemperatura();
+
+        controller.setMainUI(mainUI);
+        controller.setVentanaTemperatura(ventanaTemperatura);
+        mainUI.setController(controller);
+        ventanaTemperatura.setController(controller);
+        
+        mainUI.setVisible(true);
        
+        SerialPort puerto = new SerialPort();
+        List<String> listaPuertos;
+        Com com5;
+        String recibido = "";
+        String caracter;
+        Parameters settings = new Parameters();
+        double celsius;
+        
+        listaPuertos = puerto.getFreeSerialPort(); //Obtener puertos disponibles y enlistarlos
+        puertosDisponibles(listaPuertos);
+        settings.setPort("COM5"); //Configurar 
+        settings.setBaudRate("115200");
+        
+        com5 = new Com(settings); //Crear nuevo COM
+        
+        while(true){
+            recibido = "";
+            while(!recibido.endsWith("\n")){ //Concatenar 3 veces (CDU)
+                caracter = com5.receiveSingleString();
+                recibido += caracter;
+            }
+            Thread.sleep(100);
+        }  
+        
+        
     }
     
 }

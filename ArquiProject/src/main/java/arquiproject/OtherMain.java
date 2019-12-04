5f6371e3010c23dde4a7213d0c3bfdd6b520bb39
alextorrer <alextorre0427@gmail.com>
2019-12-04 13:21:07 -0600

    leer serial desde el main

--- ArquiProject/src/main/java/arquiproject/OtherMain.java
+++ ArquiProject/src/main/java/arquiproject/OtherMain.java
@@ -8,6 +8,9 @@ package arquiproject;
 import Controller.Controller;
 import UI.MainUI;
 import UI.VentanaTemperatura;
+import app.Com;
+import app.Parameters;
+import core.SerialPort;
 
 /**
  *
@@ -18,9 +21,18 @@ public class OtherMain {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws Exception{
         
+        //Serial
+        String serial;
+        String caracter;
+        SerialPort puerto = new SerialPort();
+        Com com5 = null;
+        Parameters settings = new Parameters();
+            settings.setPort("COM5");  
+            settings.setBaudRate("115200");
+        
+        //UI
         Controller controller = new Controller();
         MainUI mainUI = new MainUI();
         VentanaTemperatura ventanaTemperatura = new VentanaTemperatura();
@@ -30,12 +42,22 @@ public class OtherMain {
         mainUI.setController(controller);
         ventanaTemperatura.setController(controller);
         
-        String serial = "255215000";
-        controller.setSerial(serial);
-        
         mainUI.setVisible(true);
         
+        while(true){
+            serial = "";
+            while(!serial.endsWith("\n")){ 
+                caracter = com5.receiveSingleString();
+                serial += caracter;
+            }
+            Thread.sleep(100);
+            controller.setSerial(serial);
+        }  
+    
+        //serial = "255215000";
         
+        
+             
     }
     
 }
